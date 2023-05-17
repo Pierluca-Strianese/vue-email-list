b1994da5314829f174a0eb404c0b8dd898cedd43
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-05-17 09:52:52 +0200

    email printing

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const app = Vue.createApp ({
     created() {
         for (i = 0; i < 10; i++){
             axios.get ('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(Serverresponse => console.log(Serverresponse.data.response))
+            .then(serverResponse => this.arrMails.push(serverResponse.data.response))
         }
     },
 
